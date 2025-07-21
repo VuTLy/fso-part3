5c35500ea0421ad8e55385133ec03f600ff3664d
VuTLy <vulyccschool@gmail.com>
2025-07-20 20:46:16 -0700

    test deploy part 3

---
+++ phonebook_backend/index.js
@@ -0,0 +1,111 @@
+const getFormattedLocalTime = require('./utils/timeFormatter')
+const generateId = require('./utils/generateId')
+
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+
+app.use(express.json())
+
+// Create a custom token for logging the request body (only for POST)
+morgan.token('body', (req) => {
+    return req.method === 'POST' ? JSON.stringify(req.body) : '';
+});
+  
+// Use a custom format that includes the body token
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'));
+  
+  
+
+let persons = [
+    { 
+      "id": "1",
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": "2",
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": "3",
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": "4",
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.get('/', (request, response) => {
+    response.send('<h1>YES!</h1>')
+})
+
+app.get('/api/persons', (request, response) => {
+    response.json(persons)
+})
+
+app.get('/api/info', (request, response) => {
+    const time = getFormattedLocalTime()
+    const info = (`Phonebook has info for ${persons.length}<br><br>${time.replace(/,|at/g, '')}`)
+    res.set('Content-Type', 'text/html')
+    response.send(info)
+    
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    const person = persons.find(person => person.id === id)
+    
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
+
+app.delete('/api/persons/:id', (request, response) => {
+    const id = request.params.id
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+})
+
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+    
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
+
+    const nameExists = persons.some(
+        person => person.name.toLowerCase() === body.name.toLowerCase()
+    )
+
+    if (nameExists) {
+        return response.status(400).json({
+            error: 'name must be unique'
+        })
+    }
+
+    const person = {
+        id: generateId(persons),
+        name: body.name,
+        number: body.number,
+    }
+
+    persons = persons.concat(person)
+
+    response.json(person)
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
+
